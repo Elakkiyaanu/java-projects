2bda29a50dbd90a4bb3b0a6fbc0ae551a515c3f8
Elakkiyaanu <elakkiyalea@gmail.com>
2018-05-30 14:18:46 +0530

    switchcase basics

---
+++ switchcase.java
@@ -0,0 +1,22 @@
+package com.basic;
+
+public class switchcase {
+public static void main(String []args)
+{
+	int choice=30;
+	switch(choice)
+	{
+	case 10:
+		System.out.println(10);
+		break;
+	case 20:
+		System.out.println(20);
+		break;
+	case 30:
+		System.out.println(30);
+		default:
+			System.out.println("default");
+			break;
+	}
+}
+}
