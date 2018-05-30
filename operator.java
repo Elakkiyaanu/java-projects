216b9116e5622f44b1b55c46de6219e89212129b
Elakkiyaanu <elakkiyalea@gmail.com>
2018-05-30 12:49:59 +0530

    Ternary Example

---
+++ operator.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class operator 
+{
+	public static void main(String[] args)
+	{
+	      
+	        int a=2;  
+	        int b=5;  
+	        int min=(a<b)?a:b;  
+	        System.out.println(min);  
+	         
+		
+
+}
+}
