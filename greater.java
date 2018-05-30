d1e284230865ebbcdaa5832be705a5bef7f81a3d
Elakkiyaanu <elakkiyalea@gmail.com>
2018-05-30 11:22:35 +0530

    IfElseExample

---
+++ greater.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class greater
+{
+public static void main(String[] args) {
+
+int a=20;
+if(a<=10)
+{ 
+	System.out.println("a is smaller");
+
+}
+else
+{
+	System.out.println("a is greater");
+}
+}
+}
