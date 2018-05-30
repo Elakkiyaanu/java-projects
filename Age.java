115ba5ab9bd6731073010a6ab3aa73cde119bbc4
Elakkiyaanu <elakkiyalea@gmail.com>
2018-05-30 10:06:54 +0530

    Basic Example

---
+++ Age.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class Age 
+{
+ public static void main(String[] args)
+ {
+ int Age=50;
+ if ( Age >= 65 )
+System.out.println("Age is greater than or equal to 65" );
+else
+System.out.println( "Age is less than 65");
+}
+}
