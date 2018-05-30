5af0050c64322c008e77498e56a0fd6c9add49b1
Elakkiyaanu <elakkiyalea@gmail.com>
2018-05-30 14:53:36 +0530

    whiletotal example

---
+++ whiletotal.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class whiletotal {
+
+	public static void main(String []args)
+	{
+		int x=1,total=0;
+	
+while ( x <= 100 )
+{
+total += x;
+++x;
+System.out.println(total);
+}
+}
+}
