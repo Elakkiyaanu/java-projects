b7920a9ef0b146d09cce69b3acea189b8552c01d
Elakkiyaanu <elakkiyalea@gmail.com>
2018-05-30 18:51:53 +0530

    do while basics 1

---
+++ dowhilebaisc1.java
@@ -0,0 +1,29 @@
+package com.basic;
+
+public class dowhilebaisc1 
+{
+	public static void main()
+	{
+int i = 1;
+double k;
+while ( i <= 10 );
+++i;
+for (k=0.1; k<= 1.0; k+= 0.1 );
+System.out.println( k );
+
+int n = 2;
+switch (n)
+{
+case 1:
+System.out.println( "The number is 1" );
+case 2:
+System.out.println( "The number is 2" );
+break;
+default:
+System.out.println( "The number is not 1 or 2" );
+break;
+}
+while (n< 10 )
+System.out.println( n++ );
+	}
+}
