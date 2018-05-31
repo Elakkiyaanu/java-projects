8f4bb13ade6a57c06ad4ff70d274b0a349c29c42
Elakkiyaanu <elakkiyalea@gmail.com>
2018-05-31 13:07:35 +0530

    class basic Example

---
+++ Admin2.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class Admin2 {
+	public static void main(String args[]) {
+	Date d1=new Date();
+	d1.setDay(31);
+	d1.setMonth(5);
+	d1.setYear(2018);
+	d1.displayDate();
+	Date d2=new Date(1,2018,6);
+	d2.displayDate();
+
+}
+
+}
